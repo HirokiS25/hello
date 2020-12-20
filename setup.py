4fb4b16223c3fbebb873113f3327648b3e386074
usr <usr@usrnoMacBook-Air.local>
2020-12-21 02:35:34 +0900

    2nd commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='hello',
+    version='0.0',
+    author='y',
+    author_email='hogehoge@hoge',
+    description='Say',
+    url='https://hoge.jp',
+    packages=['hello_world'],
+    package_dir={'hello_world': 'src/hello_world'},
+    python_requires='>=3.8'
+)
